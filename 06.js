95a436992f21cc9aa1800e7db2f873995bdddb3f
Azam6169 <azam.shirani@gmail.com>
2019-01-23 00:44:34 -0500

    Objects and Arrays

---
+++ 06.js
@@ -0,0 +1,16 @@
+//Data sets,Array
+let listOfNumbers = [2, 3, 5, 7, 11];
+console.log(listOfNumbers[2]);
+// → 5
+console.log(listOfNumbers[0]);
+// → 2
+console.log(listOfNumbers[2 - 1]);
+// → 3
+//Properties
+//Almost all JavaScript values have properties. The exceptions are null and undefined
+let  name =" Mina";
+let  n = name.length;
+console.log(n);
+
+
+
