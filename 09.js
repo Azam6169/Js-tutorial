f6def58c6c07f6b88ff115829f5240e1f92871e0
Azam6169 <azam.shirani@gmail.com>
2019-02-20 21:34:19 -0500

    Loop While

---
+++ 09.js
@@ -0,0 +1,11 @@
+//The do/while loop is a variant of the while loop. This loop will execute the code block once, before checking if the condition is true, then it will repeat the loop as long as the condition is true.
+var cars = ["BMW", "Volvo", "Saab", "Ford"];
+var i = 0;
+var text = "";
+
+for (;cars[i];) {
+  text += cars[i] + "<br>";
+  i++;
+}
+console.log(text);
+
