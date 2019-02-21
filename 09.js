ae9cfec9622ca3e54c86863d304c4a90682bff56
Azam6169 <azam.shirani@gmail.com>
2019-02-20 21:38:33 -0500

    Loop While

--- 09.js
+++ 09.js
@@ -9,3 +9,13 @@ for (;cars[i];) {
 }
 console.log(text);
 
+var cars = ["BMW", "Volvo", "Saab", "Ford"];
+var i = 0;
+var text = "";
+
+while (cars[i]) {
+  text += cars[i] + "<br>";
+  i++;
+}
+console.log(text);
+
